257cd9d730cd5999af54f81c69cb22c615a672c0
AnnaTupota <125197669+AnnaTupota@users.noreply.github.com>
2023-04-16 21:24:12 +0600

    Update bottyperl.py

--- bottyperl.py
+++ bottyperl.py
@@ -1,3 +1,15 @@
-from aiogram.utils import executor
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+from main import *
+from main import dp
+from aiogram import executor
+#TOKEN = "6080032473:AAEUUDw0cB_r8ppZ0I28-o7x_mH44jkUIDs"
+#bot = Bot(token=TOKEN)
+#dp = Dispatcher(bot)
+if __name__ == '__bottyperl__':
+   executor.start_polling(dp, skip_updates=True)
+
+
+
+#Параметр skip_updates=True позволяет пропустить накопившиеся входящие сообщения, если они нам не важны
+#настроить получение сообщений от сервера в Telegram. Если этого не сделать, то мы не получим ответы бота.
+#Реализовать получение новых сообщений можно с помощью поллинга.
+#Он работает очень просто — метод start_polling опрашивает сервер, проверяя на нём обновления. Если они есть, то они приходят в Telegram.
