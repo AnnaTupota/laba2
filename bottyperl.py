d18b3de5a6670df0d4b3c38a0cfd88e743df199f
AnnaTupota <125197669+AnnaTupota@users.noreply.github.com>
2023-04-17 16:29:51 +0600

    Update bottyperl.py

--- bottyperl.py
+++ bottyperl.py
@@ -1,13 +1,7 @@
-from main import *
 from main import dp
 from aiogram import executor
-#TOKEN = "6080032473:AAEUUDw0cB_r8ppZ0I28-o7x_mH44jkUIDs"
-#bot = Bot(token=TOKEN)
-#dp = Dispatcher(bot)
 if __name__ == '__bottyperl__':
    executor.start_polling(dp, skip_updates=True)
-
-
 
 #Параметр skip_updates=True позволяет пропустить накопившиеся входящие сообщения, если они нам не важны
 #настроить получение сообщений от сервера в Telegram. Если этого не сделать, то мы не получим ответы бота.
